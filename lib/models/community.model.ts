6968243479907e92fa4828e8c435c80f21862d43
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2023-09-11 16:38:12 -0400

    fix community model typo

--- lib/models/community.model.ts
+++ lib/models/community.model.ts
@@ -25,4 +25,4 @@ const communitySchema = new mongoose.Schema({
 });
 
 const Community = mongoose.models.Community || mongoose.model('Community', communitySchema);
-export default User;
+export default Community;
